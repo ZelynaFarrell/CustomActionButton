ec04dacb426400669185b04c0510fd588a2f3c33
Zelyna Farrell <zsillas@yahoo.com>
2023-08-21 18:45:52 -0700

    Initial Commit

---
+++ CustomActionButton/CustomActionButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomActionButtonApp.swift
+//  CustomActionButton
+//
+//  Created by Zelyna Sillas on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomActionButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
